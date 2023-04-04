8bd4cf581c6de931d6a10af7a30c27155aa0a5dc
ekoeryanto <ekoeryanto@gmail.com>
2023-04-05 02:07:21 +0700

    chore: not found page

---
+++ src/pages/[...all].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <IonPage>
+    <IonContent :fullscreen="true">
+      <div class="centralize">
+        <h2>
+          Page Not Found
+        </h2>
+        <div>
+          <IonBackButton default-href="/" text="Kembali" />
+        </div>
+      </div>
+    </IonContent>
+  </IonPage>
+</template>
